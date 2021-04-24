bf65a7db4669c97442e97f05304f28c3dbe9472a
Sumanth Chowdary <acforotheruse@gmail.com>
2021-04-24 16:24:32 +0530

    Index to home redirect added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,9 @@
 import React from "react";
+import { useRouter } from "next/router";
+import { PAGE_URLS } from "@Constants/urls";
 const Intro = () => {
+	const router = useRouter();
+	if (typeof window !== "undefined") router.push(PAGE_URLS.homePage);
 	return null;
 };
 export default Intro;
