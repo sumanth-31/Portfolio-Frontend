c27f4456fdadad48018079fc2583f218e0d4559f
Sumanth Chowdary <acforotheruse@gmail.com>
2021-04-28 16:31:59 +0530

    Used Alternate Fix For Navbar

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -15,51 +15,49 @@ const NavbarComponent = (props: INavbarProps) => {
 	return (
 		<div className="navbar navbar-expand-lg navbar-dark bg-dark w-100">
 			<a className="navbar-brand">Portfologger</a>
-			{showProfile
-				? [
-						<button
-							className="navbar-toggler"
-							data-toggle="collapse"
-							data-target="#collapsible"
-						>
-							<span className="navbar-toggler-icon" />
-						</button>,
-						<div className="collapse navbar-collapse" id="collapsible">
-							<ul className="navbar-nav ml-auto">
-								<li className={`nav-item ${page === "HOME" ? "active" : null}`}>
-									<Link href={PAGE_URLS.homePage}>
-										<a className="nav-link">Home</a>
-									</Link>
-								</li>
-								<li
-									className={`nav-item ${page === "POSTS" ? "active" : null}`}
+			{showProfile ? (
+				<>
+					<button
+						className="navbar-toggler"
+						data-toggle="collapse"
+						data-target="#collapsible"
+					>
+						<span className="navbar-toggler-icon" />
+					</button>
+					<div className="collapse navbar-collapse" id="collapsible">
+						<ul className="navbar-nav ml-auto">
+							<li className={`nav-item ${page === "HOME" ? "active" : null}`}>
+								<Link href={PAGE_URLS.homePage}>
+									<a className="nav-link">Home</a>
+								</Link>
+							</li>
+							<li className={`nav-item ${page === "POSTS" ? "active" : null}`}>
+								<Link href={PAGE_URLS.myPostsPage}>
+									<a className="nav-link">Posts</a>
+								</Link>
+							</li>
+							<li className="nav-item dropdown">
+								<a
+									className="nav-link dropdown-toggle "
+									data-toggle="dropdown"
+									id="profileDropDown"
+									role="button"
 								>
-									<Link href={PAGE_URLS.myPostsPage}>
-										<a className="nav-link">Posts</a>
-									</Link>
-								</li>
-								<li className="nav-item dropdown">
-									<a
-										className="nav-link dropdown-toggle "
-										data-toggle="dropdown"
-										id="profileDropDown"
-										role="button"
-									>
-										Profile
-									</a>
-									<div
-										className="dropdown-menu dropdown-menu-right mr-1"
-										aria-labelledby="profileDropDown"
-									>
-										<button className="dropdown-item" onClick={logout}>
-											Logout
-										</button>
-									</div>
-								</li>
-							</ul>
-						</div>,
-				  ]
-				: null}
+									Profile
+								</a>
+								<div
+									className="dropdown-menu dropdown-menu-right mr-1"
+									aria-labelledby="profileDropDown"
+								>
+									<button className="dropdown-item" onClick={logout}>
+										Logout
+									</button>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</>
+			) : null}
 		</div>
 	);
 };
